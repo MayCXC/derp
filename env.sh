40da48c4f22a4bd22dabcf480768bc0ed1c7d7a4
maycxc.github.io <9441877+MayCXC@users.noreply.github.com>
2024-10-07 17:20:02 -0400

    no tac

--- env.sh
+++ env.sh
@@ -37,59 +37,6 @@ envf envp <<-'EOT'
 	PS1="\n${PS1}"
 	EOT
 
-# todo getopts etc
-if ! command -v tac >/dev/null; then
-	envf tac <<-'EOT'
-		TAC_FLAG_B=
-		TAC_FLAG_R=
-		TAC_FLAG_S=
-		eval "$(
-			envl OPTIND OPTARG OPT OPTLONG <<-'EOT_'
-				while getopts "brs:-:" OPT; do
-					if [ ! "${OPTLONG-o}" = "${OPTLONG-x}" ]; then
-						OPT="${OPTLONG}"
-						unset OPTLONG
-					else
-						case "${OPT}" in
-							(-*=*) IFS="=" read OPTLONG OPTARG <<EOT__
-								${OPT#"-"}
-								EOT__
-								;;
-							(*=*);;
-							(-*) OPT="${OPT#"-"}";;
-							(*) ;;
-						esac
-					fi
-					if [ "${OPT}" = "-" ]; then
-						while IFS="=" read -r OPTLONG OPTARG
-						<<EOT__
-							${OPTARG#"-"}
-							EOT__
-					fi
-					case "${OPT}" in
-						();;
-					esac
-				done
-				EOT_
-		)"
-		shift $(($OPTIND - 1))
-		cat "$@" | {
-			while IFS= read -r L; do
-				set -- "${L}" "$@"
-			done
-			printf "%s\n" "$@"
-		}
-		if [ $# -eq 0 ]; then
-			while IFS= read -r L; do
-				set -- "${L}" "$@"
-			done
-			printf "%s\n" "$@"
-		else
-			cat "$@" | tac
-		fi
-		EOT
-fi
-
 if [ $# -eq 0 ]; then
 	eval "$(
 		envl IFS <<-'EOT'
